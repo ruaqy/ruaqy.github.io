0282ea5d88b306fccbda4f481aaa1fc56730bbb9
quanyuan ruan <ruanquanyuan@outlook.com>
2021-08-31 21:32:56 +0800

    add pages Signed-off-by: quanyuan ruan <ruanquanyuan@outlook.com>

---
+++ js/subpage.js
@@ -0,0 +1,19 @@
+function PageHome () {
+	var iframe = document.getElementById("subpage")
+	iframe.src = "./home.html";
+}
+
+function PageArtist(){
+	var iframe = document.getElementById("subpage")
+	iframe.src = "./artist.html";
+}
+
+function PageProject(){
+	var iframe = document.getElementById("subpage")
+	iframe.src = "./project.html";
+}
+
+function PageAbout () {
+	var iframe = document.getElementById("subpage")
+	iframe.src = "./about.html";
+}
