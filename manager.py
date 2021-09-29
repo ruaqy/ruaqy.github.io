96dde99a94a58777f06a92387060c58e992c754a
quanyuan ruan <ruanquanyuan@outlook.com>
2021-09-29 17:43:53 +0800

    更新界面

--- manager.py
+++ manager.py
@@ -1,122 +1,97 @@
 import os, re, sys, time, json, shutil, getopt, logging
 
 
-class WebManager:
-    def __init__(self):
+class SiteManager:
+    def __init__(self, state):
         # 打开数据库
-        data_name = './data/dt.json'
-        if os.path.isfile(data_name):
-            with open(data_name, encoding="utf-8") as dt:
+        # dt_arti.json文章数据库
+        # dt_proj.json项目数据库
+        # dt_home.json首页数据库
+        self.state = state
+        
+        urls = ['./data/dt_arti.json', './data/dt_proj.json', 
+                './data/dt_home.json' ]
+        state = ["A", "P", "H"]
+        
+        if self.state in state:
+            self.data_name = urls[state.index(self.state)]
+            
+        if os.path.isfile(self.data_name):
+            with open(self.data_name, encoding="utf-8") as dt:
                 self.web_dt = json.load(dt)
-                logging.debug("Database existed, readed.")
+                logging.debug("数据库存在,加载中.")
         else:
             self.web_dt = {}
-            logging.debug("Database does not exist, created.")
-    
-    def __del__(self):
-        # 退出前保存
-        # self.save()
-        pass
-    
-    # 保存数据库    
+            logging.debug("数据库不存在,已创建.")
+            
     def save(self):
-        data_name = r'./data/dt.json'
-        with open(data_name, 'w', encoding="utf-8") as dt:
+        with open(self.data_name, 'w', encoding="utf-8") as dt:
             json.dump(self.web_dt, dt)
-        logging.debug("Database parser completed.")
-
-
+            print(self.web_dt)
+            
+        logging.debug("数据库已保存.")
+        
     # 根据文件编译数据库
     def parser_data(self):
         # 读取文章写入数据库
         def read_article():
             files = os.listdir(r'./pages/post/')
+            # 文章数据
             articles = []
             for out_file in files:
-                # 获得文件修改时间
-                filemt= time.strftime('%Y-%m-%d', time.localtime(os.stat("./pages/post/{}".format(out_file)).st_ctime))
+                # 创建时间
+                ctime = os.stat("./pages/post/{}".format(out_file)).st_ctime
+                # 修改时间
+                mtime = os.stat("./pages/post/{}".format(out_file)).st_mtime
+                
+                # filemt= time.strftime('%Y-%m-%d', time.localtime())
+                
+                # 标题 概要
                 title, main_text = "", ""
                 with open("./pages/post/{}".format(out_file), encoding="utf-8") as article:
+                    # 读取标题
                     title = article.readline()
                     title = re.match("#+ (.+)", title)
+                    
                     if title:
                         title = title.group(1)
                     else:
-                        logging.warning("Not match title, post:{}".format(out_file))                    
+                        logging.warning("无标题:{}".format(out_file))                    
                     main_text = article.read(60)
+                    
                     if main_text:
                         main_text = re.sub(r'\s+', '', main_text)
                     else:
-                        logging.warning("Not match text, post:{}".format(out_file))                    
-                    logging.debug("Parser text finished:{}".format(out_file))
+                        logging.warning("无正文:{}".format(out_file))       
+                        
+                    logging.debug("文本解析完成:{}".format(out_file))
                     
-                    articles.append({"title": title, 
-                                     "url":out_file,
-                                     "main":main_text,
-                                     "time":filemt})
-            self.web_dt["article"] = articles
-        read_article()
-        
-    def connect_file(self):
-        def write_home():
-            # 生成文章页
-            out_text = '# 主页\n\n'
-            with open("pages/home.md", 'w', encoding="utf-8") as f:
-                recent_post = '## 近期文章\n'
-                post = self.web_dt.get("article", [])
-                if post:
-                    append_text = """---
-### [{}](./post.html#pages/post/{})
-
-{}
-
-**{}**
-    
-""".format(post[-1].get("title", ""), post[-1].get("url", ""), post[-1].get("main", ""), post[-1].get("time", ""))
-                recent_post += append_text
-                
-                recent_proj = '## 近期项目\n'
-                proj = self.web_dt.get("proj", [])
-                if proj:
-                    recent_proj += "\n"+proj[-1]+"\n"
-                
-                f.write(out_text + recent_post + recent_proj)
+                    # | t_t  | 文章标题 |
+                    # | m_c  | 文章概览 |
+                    # | c_t  | 创建时间 |
+                    # | m_t  | 修改时间 |
+                    # | url  | 文章地址 |
+                    
+                    articles.append({"t_t": title, 
+                                     "m_c":main_text,
+                                     "c_t":ctime,
+                                     "m_t":mtime,
+                                     "url":"./post.html#pages/post/{}".format(out_file)})
+                    
+            articles = sorted(articles, key=lambda x:x.get("c_t", 0), reverse=True)
             
-            logging.debug("Home Publish Completed.")
-                
-        def write_article():
-            out_text = '# 文章\n'
-            with open("pages/article.md", 'w', encoding="utf-8") as f:
-                # 获取数据
-                articles = self.web_dt.get("article", [])
-                # 倒序输出
-                for index in range(len(articles)-1, -1, -1):
-                    item = articles[index]
-                    append_text = """---
-### [{}](./post.html#pages/post/{})
-
-{}
-
-**{}**
+            
+            
+            for article in articles:
+                article["c_t"] = time.strftime('%Y-%m-%d', time.localtime(article.get("c_t", 0)))
+                article["m_t"] = time.strftime('%Y-%m-%d', time.localtime(article.get("m_t", 0)))
+            
+            # 重建数据库
+            self.web_dt["article"] = articles 
     
-""".format(item.get("title", ""), item.get("url", ""), item.get("main", ""), item.get("time", ""))
-                    out_text += append_text
-                logging.debug("Article Publish Completed.")
-                f.write(out_text)
-                
-        def write_project():
-            out_text = '# 项目\n'
-            with open("pages/project.md", 'w', encoding="utf-8") as f:
-                # 获取数据
-                for item in self.web_dt.get("proj", []):
-                    out_text += "\n" + item + "\n"
-                logging.debug("Project Publish Completed.")
-                f.write(out_text)
-    
-        write_home()
-        write_article()
-        write_project()
-        
+        if self.state == "A":
+            read_article()
+            
     def move_draft(self):
         # 移动草稿
         def read_draft():
@@ -142,28 +117,33 @@ class WebManager:
                     logging.error("Please input the number.")            
         read_draft()
         
-    def add_proj(self, proj):
-        proj_ls = self.web_dt.get("proj", [])
-        if proj in proj_ls:
-            logging.info("Project really exist:{}".format(proj))
-        else:
-            proj_ls.append(proj)
-            # self.web_dt["proj"] = proj_ls
-            logging.info("Project Has Log:{}".format(proj))
+    def add_proj(self):
+        proj_ls = self.web_dt.get("project", [])
+        t_t = input("请输入项目标题")
+        m_c = input("请输入项目概览")
+        p_s = input("请输入项目状态")
+        p_l = input("请输入项目链接")
+        p_p = input("请输入项目备注")
+        proj_ls.append({"t_t": t_t, "m_c": m_c, "p_s": p_s, "p_l": p_l, "p_p": p_p})
+        self.web_dt["project"] = proj_ls
     
     def remove_proj(self):
-        proj_ls = self.web_dt.get("proj", [])
-        if not proj_ls:
-            logging.info("Project does not have any thing.")
-            sys.exit(0)
-        for index in range(len(proj_ls)):
-            print(index, proj_ls[index])
-        index = input("please press number of which you want to remove.")
-        if index.isnumeric():
-            proj_ls.pop(int(index))
-            logging.info("Project remove finish.")
-        else:
-            logging.error("Please input the number")
+        proj_ls = self.web_dt.get("project", [])
+        # | t_t  | 项目标题 |
+        # | m_c  | 项目概览 |
+        # | p_s  | 项目状态 |
+        # | p_l  | 项目链接 |
+        # | p_p  | 项目备注 |
+        t_t = input("请输入项目标题")
+        m_c = input("请输入项目概览")
+        p_s = input("请输入项目状态")
+        p_l = input("请输入项目链接")
+        p_p = input("请输入项目备注")
+        proj_ls.append({"t_t": t_t, "m_c": m_c, "p_s": p_s, "p_l": p_l, "p_p": p_p})
+        print(proj_ls)
+        print(123)
+        self.web_dt["project"] = proj_ls
+    
     
 def main(argv):
     if not argv:
@@ -174,24 +154,23 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv,"a:rmpbgh",["help", "remove","add=",
                                                    "move","parser","build",
-                                                   "generate"])
+                                                   "generate", "pa"])
     except getopt.GetoptError:
         logging.error("Command error.Press -h or -help to get help.")
         sys.exit(1)
-
-    manager = WebManager()
-
+        
     for opt, arg in opts:
         if opt in ('-h', "--help"):
             # 帮助文件
             print("""
-usage: python manager.py [option] [arg] ...
-Options and arguments:
-    -a or --add      : add the project of web site.use:
-                       -a arg
-                       --add arg
-                       you need to add arg to the command. 
+用法: python manager.py [option] [arg] ...
+选项 与 变量:
+    -a or --add      : 添加文章
+                       -a [文章名]
+                       --add [文章名]
+                       文章名不是一个可选项
     -r or --remove   : remove the project of web site.
+    
     
     
     -c or --create   : write markdown draft.
@@ -201,15 +180,23 @@ Options and arguments:
     -p or --parser   : parser the database.
     -b or --build    : bulid the markdown file.
     -g or --generate : generate the database and the markdown file.
-                  """)
+
+    --pa           :添加项目 
+
+              """)
             sys.exit(0)
             
         if opt in ('-a', "--add"):
+            manager = SiteManager("P")
+
             manager.add_proj(arg)
             manager.save()
             sys.exit(0)
                     
         if opt in ('-r', "--remove"):
+            manager = SiteManager("P")
+
+            
             manager.remove_proj()
             manager.save()
             sys.exit(0)
@@ -219,17 +206,28 @@ Options and arguments:
             sys.exit(0)
             
         if opt in ('-p', "--parser"):
+            manager = SiteManager("A")
+            
             manager.parser_data()
             manager.save()
             sys.exit(0)
             
         if opt in ('-b', "--build"):
-            manager.connect_file()
+            manager = SiteManager("P")
+            
             sys.exit(0)
             
         if opt in ('-g', "--generate"):
+            manager = SiteManager("P")
+            
             manager.parser_data()
-            manager.connect_file()
+            manager.save()
+            sys.exit(0)
+            
+        if opt in ("--pa"):
+            manager = SiteManager("P")
+            
+            manager.add_proj()
             manager.save()
             sys.exit(0)
    
@@ -240,4 +238,6 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     
     main(sys.argv[1:])
+
+
 
--- pages/project.md
+++
@@ -1,9 +0,0 @@
-# 项目
-
-[![RQY/RuaBlog](https://gitee.com/muronglengjing/rua-blog/widgets/widget_card.svg?colors=4183c4,ffffff,ffffff,e3e9ed,666666,9b9b9b)](https://gitee.com/muronglengjing/rua-blog)
-
-[![RQY/ST7735S LCD驱动](https://gitee.com/muronglengjing/st7735-s-lcd-driver/widgets/widget_card.svg?colors=4183c4,ffffff,ffffff,e3e9ed,666666,9b9b9b)](https://gitee.com/muronglengjing/st7735-s-lcd-driver)
-
-[![RQY/学生信息管理系统](https://gitee.com/muronglengjing/my_student_manager/widgets/widget_card.svg?colors=4183c4,ffffff,ffffff,e3e9ed,666666,9b9b9b)](https://gitee.com/muronglengjing/my_student_manager)
-
-[![RQY/DjangoOnlineShop](https://gitee.com/muronglengjing/django-online-shop/widgets/widget_card.svg?colors=4183c4,ffffff,ffffff,e3e9ed,666666,9b9b9b)](https://gitee.com/muronglengjing/django-online-shop)
